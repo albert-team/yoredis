c1f028a9b07d2ae842077af50071c2dc93b612b1
Luong Quang Manh <luongquangmanh85@gmail.com>
2019-10-31 00:13:35 +0700

    Update testing script

--- tests/index.js
+++ tests/index.js
@@ -3,19 +3,19 @@ const Redis = require('..')
 const main = async () => {
   const client = new Redis()
 
-  console.log(client.ready)
+  console.log(client.ready) // false
   await client.connect()
-  console.log(client.ready)
+  console.log(client.ready) // true
 
-  console.log(await client.call('PING'))
-  console.log(await client.callOne(['GET', 'key1']))
+  console.log(await client.call('PING')) // PONG
+  console.log(await client.callOne(['GET', 'key1'])) // null
   console.log(
     await client.callMany([['SET', 'key1', 'val1'], ['GET', 'key1'], ['GET', 'key2']])
-  )
+  ) // [ 'OK', 'val1', null ]
 
-  console.log(client.disconnected)
+  console.log(client.disconnected) // false
   await client.disconnect()
-  console.log(client.disconnected)
+  console.log(client.disconnected) // true
 }
 
 main()
